8b9cc43d9a0c0f552f8f167548448fbb2b598185
Hezkore <Hezkore@GMail.com>
2019-11-30 17:32:27 +0100

    Updated quickAnalyze

--- src/quickAnalyze.ts
+++ src/quickAnalyze.ts
@@ -1,25 +1,27 @@
 import * as vscode from 'vscode'
 
-export async function quickAnalyze( code: string ): Promise<QuickAnalyzeResult> {
+export async function quickAnalyze( code: string, stopAtStrict: boolean = false ): Promise<QuickAnalyzeResult> {
 	
 	return new Promise( async function( resolve ) {		
 		
-		if (!code || code.length < 6)
-			return {strict: false, imports: [], lastImportLine: 0}
-		
 		let result: QuickAnalyzeResult = { strict: false, strictType: '', framework: '', imports: [], lastImportLine: 0 }
+		if (!code || code.length < 6)
+			return result
+		
 		let lines: string[] = code.trim().split( '\n' )
 		let depth: number = 0
 		
 		for (let i = 0; i < lines.length; i++) {
 			const line = lines[i].trimRight()
-			if (line.length < 6  || line.startsWith( "'" ) || line.startsWith( "rem " ) ) continue
+			if (line.length < 3  || line.startsWith( "'" )) continue
 			const lowerLine: string = line.toLowerCase()
 			
 			if (lowerLine.startsWith( 'rem ' ))
 				depth++
 			else if (lowerLine.startsWith( 'endrem' ) || lowerLine.startsWith( 'end rem' ))
+			{
 				if (depth > 0) depth--
+			}
 			else if (depth > 0)
 				continue
 			else if (lowerLine.startsWith( 'strict' ))
@@ -42,6 +44,9 @@ export async function quickAnalyze( code: string ): Promise<QuickAnalyzeResult> 
 				result.framework = line.split( ' ' )[1]
 				result.lastImportLine = i
 			}
+			
+			if (stopAtStrict && result.strict)
+				return resolve( result )
 		}
 		return resolve( result )
 	})
